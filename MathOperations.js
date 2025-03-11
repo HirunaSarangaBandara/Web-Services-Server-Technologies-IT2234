a5f0941500468b4f84165e25807d00891e854417
Hiruna Saranga Bandara <hiruna12saranga12@gmail.com>
2025-03-11 16:02:18 +0530

    Basic Mathematical Operations
    This code demonstrates basic mathematical operations using both let and var keywords in JavaScript. It highlights addition, subtraction, multiplication, division, and modules operations. Additionally, it shows how var allows variable redeclaration, which can lead to unexpected results compared to let, which prevents redeclaration.

---
+++ MathOperations.js
@@ -0,0 +1,51 @@
+//Basic Mathematical Operations
+
+console.log("*** Basic Mathematical Operations ***")
+
+console.log("* Using let Keyword *")
+
+
+let num1 = 20;
+let num2 = 15;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let div = num1 / num2;
+let mul = num1 * num2;
+let mod = num1 % num2;
+
+console.log("Summation : " + sum)
+console.log("Substraction : " + sub)
+console.log("Division : " + div)
+console.log("Multiplication : " + mul)
+console.log("Modules : " + mod)
+
+/* Output : 
+	*** Basic Mathematical Operations ***
+	* Using let Keyword *
+	Summation : 35
+	Substraction : 5
+	Multiplication : 300
+	Modules : 5
+*/
+
+console.log("* Using var Keyword *")
+
+var Num1 = 12
+var Num2 = 8
+var Num2 = 5
+
+var add = Num1 + Num2 + Num2;
+var subs = Num1 - Num2;
+
+console.log("Summation : " + add)
+console.log("Summation : " + subs)
+
+/* Output : 
+	*** Basic Mathematical Operations ***
+	* Using let Keyword *
+	Summation : 35
+	Substraction : 5
+	Multiplication : 300
+	Modules : 5
+*/
