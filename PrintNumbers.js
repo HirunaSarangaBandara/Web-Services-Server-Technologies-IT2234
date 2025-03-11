978460ae154f27f211e4c718ad9787833e890e64
Hiruna Saranga Bandara <hiruna12saranga12@gmail.com>
2025-03-11 16:17:51 +0530

    Print Numbers Using Loops
    This code simply prints numbers from 1 to 10 using two different loops: a for loop and a while loop. The for loop is great when you know how many times to repeat, while the while loop keeps going until a condition is met.

---
+++ PrintNumbers.js
@@ -0,0 +1,43 @@
+//Print Numbers Using Loops
+
+console.log("*** Print Numbers 1 to 10 ***")
+console.log("* Using for Loop *")
+
+for(var i = 1; i <= 10; i++){
+	console.log(i)
+}
+
+console.log("* Using while Loop *")
+
+var j = 1;
+
+while(j <= 10){
+	console.log(j)
+	j++;
+}
+
+/* Output
+	*** Print Numbers 1 to 10 ***
+	* Using for Loop *
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+	10
+	* Using while Loop *
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+	10
+*/
