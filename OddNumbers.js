6f62a30ec08582a776d5f01b4bb244a7d7a0c598
Hiruna Saranga Bandara <hiruna12saranga12@gmail.com>
2025-03-11 16:43:33 +0530

    Print Only Odd Numbers
    This code prints only the odd numbers from 1 to 10. It loops through the numbers and checks if each one is odd using the % operator. If the number isn't evenly divisible by 2, it gets printed.

---
+++ OddNumbers.js
@@ -0,0 +1,21 @@
+//Print only odd numbers
+
+console.log("*** Print only odd numbers ***");
+
+for(var j=1; j<=10; j++)
+{
+	if(j % 2 == 1)
+	{
+		console.log(j);
+	}
+}
+
+/* Output
+	*** Print only odd numbers ***
+	1
+	3
+	5
+	7
+	9
+*/
+
