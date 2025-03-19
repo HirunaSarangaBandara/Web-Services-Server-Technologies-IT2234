7fbc7c1455d89e264ac2f557b7076ddda9966525
Hiruna Saranga Bandara <hiruna12saranga12@gmail.com>
2025-03-19 14:22:01 +0530

    JavaScript Object Notation
    The code defines a JSON-like object student with personal details and skills, then logs its properties. It also creates an array students containing multiple student objects and prints them.

---
+++ JSON.js
@@ -0,0 +1,15 @@
+console.log("--- JSON ---")
+
+//{key : value}
+
+let student = {regNo : '2021/ICT/81', name : 'Hiruna', age : 21, course : 'IT', skills : ['Java', 'JS', 'C++'] }
+console.log(student)
+console.log(student.name)
+
+let students = [
+		{regNo : '2021/ICT/80', name : 'Shani', age : 25, course : 'IT'},
+		{regNo : '2021/ICT/81', name : 'Hiruna', age : 21, course : 'IT'},
+		{regNo : '2021/ICT/82', name : 'Dhanushka', age : 26, course : 'IT'}
+	]
+	
+console.log(students)
